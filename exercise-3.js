8448477db54d99e797bd6a6b3d1c04e42eb6a693
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-07-30 14:58:27 +0700

    gameplay finish

--- exercise-3.js
+++ exercise-3.js
@@ -1,2 +1,19 @@
 //Proxytia Gameplay
 
+var nama = 'Marcell';
+var peran = 'Penyihir';
+
+if (nama === '' && peran === '') {
+    console.log('Nama harus diisi!');
+} else if (nama === 'Marcell' && peran === '') {
+    console.log('Halo Marcell, Pilih peranmu untuk memulai game!');
+} else if (nama === 'Marcell' && peran === 'Ksatria') {
+    console.log('Selamat datang di Dunia Proxytia, Marcell');
+    console.log('Halo Ksatria Marcell, kamu dapat menyerang dengan senjatamu!');
+} else if (nama === 'Marcell' && peran === 'Tabib') {
+    console.log('Selamat datang di Dunia Proxytia, Marcell');
+    console.log('Halo Tabib Marcell, kamu akan membantu temanmu yang terluka.');
+} else if (nama === 'Marcell' && peran === 'Penyihir') {
+    console.log('Selamat datang di Dunia Proxytia, Marcell');
+    console.log('Halo Penyihir Marcell, ciptakan keajaiban yang membantu kemenanganmu!');
+}
