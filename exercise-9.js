b00605ca67764ff75a950ce206b1e2a51773a718
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-07-31 17:36:50 +0700

    create exercise-9

---
+++ exercise-9.js
@@ -0,0 +1,19 @@
+// MENGENAL PENGGUNAAN FUNCTION
+
+// Tugas 1
+
+function shoutOut(text1, text2){
+
+ var text1;
+ var text2;
+ var sentence;
+
+ sentence = text1 + text2;
+ 
+ return sentence;
+}
+console.log(shoutOut('Halo ', 'Function!'));
+// menampilkan "Halo Function!"
+
+
+// Tugas 2
