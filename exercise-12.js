c2b14adb6da988194e8210c0668662a43d0dad71
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-08-01 13:17:33 +0700

    done exercise-12.js

--- exercise-12.js
+++ exercise-12.js
@@ -1,9 +1,19 @@
 function konversiMenit(menit) {
-    // you can only write your code here!
-    var jam = menit/60
-    
+  var jam;
+  var menit;
+
+  jam = Math.floor(menit/60);
+  menit = menit % 60;
+
+  if (menit<10){
+    return jam + ':0' + menit
   }
-  return jam
+  else if (menit>10) {
+    return jam + ':' + menit
+  }
+}  
+
+
   // TEST CASES
   console.log(konversiMenit(63)); // 1:03
   console.log(konversiMenit(124)); // 2:04
