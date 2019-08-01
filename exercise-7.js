553542dd96745e6ba7930c247632571ecdbae074
elizabethwidiasri <elizabethwidiasri@gmail.com>
2019-08-01 14:28:31 +0700

    perbaikan exercise-7.js

--- exercise-7.js
+++ exercise-7.js
@@ -3,9 +3,9 @@
 //1. Menyusun Barisan Bintang
 
 var rows1=5;
-var i;
 
-for (i=0; i<rows1; i++) {
+
+for (var i=0; i<rows1; i++) {
     console.log('*');
 }
 console.log('\n');
@@ -13,11 +13,11 @@ console.log('\n');
 //2. Menyusun Barisan Bintang dengan Nested Looping
 
 var rows2=5;
-var k;
+
 
 for (i=0; i<rows2; i++) {
     var asterisks='';
-    for(k=0; k<rows2; k++) {
+    for(var k=0; k<rows2; k++) {
         asterisks = asterisks + '*'
     }
     console.log(asterisks);
@@ -28,10 +28,10 @@ console.log('\n')
 
 var rows3=5;
 var asterisks='';
-var l;
+
 
 for (i=0; i<rows3; i++) {
-    for(l=0; l<rows3; l+=rows3) {
+    for(var l=0; l<rows3; l+=rows3) {
         asterisks = asterisks + '*'
     }
     console.log(asterisks);
